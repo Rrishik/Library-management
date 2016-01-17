f3f66243b62619d689018559129da7935d3d049a
Rrishik <ramenarishik@gmail.com>
2016-01-17 16:46:45 +0530

    Form submit action added
    submitting the form adds the details to the array & displays in the list

--- myscript.js
+++ myscript.js
@@ -45,3 +45,13 @@ function viewBook(ISBN){
 	}
 	return false;
 }
+
+function onSubmit(){
+	var NAME = document.getElementById("addForm").Name.value;
+	var AUTHOR = document.getElementById("addForm").Author.value;
+	var PRICE = document.getElementById("addForm").Price.value;
+	var ISBN = document.getElementById("addForm").Isbn.value;
+	books.Books.push({"name":NAME,"author":AUTHOR,"price":PRICE,"isbn":ISBN});
+	loadBooks();
+	return false;
+}
